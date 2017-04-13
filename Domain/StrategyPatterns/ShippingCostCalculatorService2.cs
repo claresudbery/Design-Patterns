ac99e6631cadf791899bf72de193a4f3fd8a75cd
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-04-13 13:47:48 +0100

    new strategy stuff

---
+++ Domain/StrategyPatterns/ShippingCostCalculatorService2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.StrategyPatterns
+{
+    public class ShippingCostCalculatorService2
+    {
+        public double CalculateShippingCost(Order order, Func<Order, double> shippingCostStrategy)
+        {
+            return shippingCostStrategy(order);
+        }
+    }
+}

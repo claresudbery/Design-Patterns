808a2f9804937abb4455f788ebcb1d1be6126c2e
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-04-21 18:45:46 +0100

    changes made during / after workshop

---
+++ Design-Patterns/StrategyTests/CustomerRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain;
+using Domain.RepositoryPatterns;
+using Domain.StrategyPatterns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StrategyTests
+{
+    [TestClass]
+    public class CustomerRepositoryTests
+    {
+        [TestMethod]
+        public void CustomerWithId1IsCalledClare()
+        {
+            // Arrange
+            var customerRepository = new CustomerRepository();
+            var customerId = 1;
+
+            // Act
+            var customer = customerRepository.FindBy(customerId);
+
+            // Assert
+            Assert.AreEqual("Clare", customer.Name);
+        }
+    }
+}
